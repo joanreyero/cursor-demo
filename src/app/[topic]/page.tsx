a9460d837515b17f7344a156b76bffebe60dbd34
joanreyero <joan@reyero.io>
2024-09-09 17:14:45 +0100

    Topic page

---
+++ src/app/[topic]/page.tsx
@@ -0,0 +1,13 @@
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-2xl font-bold tracking-tight">Topic</h1>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
